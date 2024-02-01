8765bac41451e8407339bbfe7350f77f10923013
조혜민 <104076643+cho-hm02123@users.noreply.github.com>
2024-02-01 17:48:25 +0900

    Update 2493.cpp

--- Beakjoon/스택/2493.cpp
+++ Beakjoon/스택/2493.cpp
@@ -1,3 +1,13 @@
+/*
+문제 : 탑에서 레이저를 왼쪽으로 쏴 수신한 탑의 번호를 찾는 문제 (송신한 탑보다 큰 탑이 수신)
+입력 : 탑 개수, 탑 높이
+
+풀이 :
+ pair를 사용 (#include <utility>)
+pair를 통해 탑의 길이와 인덱스를 한번에 저장
+어차피 왼쪽 가장 가까이에 있는 탑이 수신하므로 높은 탑이 들어오면 앞에 pop 해서 없애줌 
+*/
+
 #include <istream>
 #include <iostream>
 #include <stack>
