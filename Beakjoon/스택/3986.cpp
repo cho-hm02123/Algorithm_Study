e8a835b3b04be5e2ab2f587318b44157dc15da2d
조혜민 <104076643+cho-hm02123@users.noreply.github.com>
2024-02-01 11:04:27 +0900

    Create 3986.cpp

---
+++ Beakjoon/스택/3986.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <istream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N = 0;
+	int i = 0;
+	cin >> N;
+
+	while (N--) {
+		stack <char> c;
+		string str;
+		cin >> str;
+
+		for (auto a : str) {
+			if (c.empty() || c.top() != a)
+				c.push(a);
+			else
+				c.pop();
+		}
+
+		if (c.empty())
+			i++;
+	}
+
+	cout << i;
+
+	return 0;
+}
