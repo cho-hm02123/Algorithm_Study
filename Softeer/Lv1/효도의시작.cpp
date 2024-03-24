0698f9128196da4733af512c65f73b24ac7dd111
조혜민 <104076643+cho-hm02123@users.noreply.github.com>
2024-03-24 21:18:37 +0900

    Create 효도의시작.cpp

---
+++ Softeer/Lv1/효도의시작.cpp
@@ -0,0 +1,14 @@
+#include <istream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << "엄마 아빠 사랑해요!";
+
+	return 0;
+}
