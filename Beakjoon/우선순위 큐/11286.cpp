a65cbba862ff1c877803f01e168f7e35131d936b
조혜민 <104076643+cho-hm02123@users.noreply.github.com>
2024-04-12 15:03:12 +0900

    Create 11286.cpp

---
+++ Beakjoon/우선순위 큐/11286.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+int n;
+
+class cmp {
+public:
+	bool operator() (int a, int b) {
+		if (abs(a) != abs(b)) return abs(a) > abs(b);
+		return a > 0 && b < 0;
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	priority_queue<int, vector<int>, cmp> pq;
+
+	cin >> n;
+
+	while (n--) {
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (pq.empty())
+				cout << "0 \n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+			pq.push(x);
+	}
+
+	return 0;
+}
