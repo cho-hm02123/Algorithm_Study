0c70a9a0adbacd35a3e01f73e5fe6c94a36e3615
조혜민 <104076643+cho-hm02123@users.noreply.github.com>
2024-05-18 23:20:41 +0900

    Create 성적평균.cpp

---
+++ Softeer/Lv3/성적평균.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+float arr[1000001];
+
+int main(int argc, char** argv)
+{
+    cin >> n >> k;
+    for (int i = 1; i < n + 1; i++)
+        cin >> arr[i];
+    while (k--) {
+        float a, b;
+        cin >> a >> b;
+        float sum = 0;
+        for (int i = a; i < b + 1; i++)
+            sum += arr[i];
+        float rst = sum / (b - a + 1);
+        rst = ceil(rst * 100) / 100.0;
+        cout.setf(ios::fixed);
+        cout.precision(2);
+        cout << rst << "\n";
+    }
+
+    return 0;
+}
